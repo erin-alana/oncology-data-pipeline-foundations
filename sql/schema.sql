e5cda8006cbaea423b399465a38f43b33c3ccc26
Erin <erinalanamorgan@yahoo.com>
2026-06-06 18:41:31 -0500

    Create patient database schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,11 @@
-
+CREATE TABLE patients (
+    patient_id TEXT PRIMARY KEY,
+    age_at_diagnosis INTEGER,
+    sex TEXT,
+    primary_site TEXT,
+    histology TEXT,
+    stage TEXT,
+    vital_status TEXT,
+    is_advanced_stage BOOLEAN,
+    age_group_at_diagnosis TEXT
+);
